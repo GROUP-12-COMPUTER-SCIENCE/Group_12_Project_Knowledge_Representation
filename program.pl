3e0279135b687d669b87a61a557c6b924c557622
Clint171 <clintsimiyu004@gmail.com>
2023-09-26 16:53:42 +0300

    Added uncle and aunt rules

--- program.pl
+++ program.pl
@@ -1,1 +1,23 @@
-swipl -s program.pl
+female(pam).
+female(liz).
+female(pat).
+female(ann).
+male(jim).
+male(bob).
+male(tom).
+male(peter).
+parent(pam,bob).
+parent(tom,bob).
+parent(tom,liz).
+parent(bob,ann).
+parent(bob,pat).
+parent(pat,jim).
+parent(bob,peter).
+parent(peter,jim).
+mother(X,Y):- parent(X,Y),female(X).
+father(X,Y):- parent(X,Y),male(X).
+haschild(X):- parent(X,_).
+sister(X,Y):- parent(Z,X),parent(Z,Y),female(X),X\==Y.
+brother(X,Y):- parent(Z,X),parent(Z,Y),male(X),X\==Y.
+uncle(X,Y):- parent(Z,X),parent(Z,W),parent(W,Y),not(parent(X,Y)),male(X).
+aunt(X,Y):- parent(Z,X),parent(Z,W),parent(W,Y),not(parent(X,Y)),female(X).
